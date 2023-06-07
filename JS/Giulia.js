d6f1a218b15c10c0c085be06546d94ec11a31192
nicrinhodela <lucasbrayonff@gmail.com>
2023-06-07 14:12:10 -0400

    Atualização

---
+++ JS/Giulia.js
@@ -0,0 +1,17 @@
+const inputs = document.querySelectorAll("[required]");
+inputs.forEach( (elemento)=>{
+    elemento.addEventListener("blur", (evento)=>{
+        validaCampo(evento.target)
+    });
+});
+
+function validaCampo(campo){
+    const msnErro = campo.parentNode.querySelector("[data-erro]");
+    if(campo.name ==="nome"){
+        if(campo.value.length<5){
+            msnErro.textContent = "digite o nome completo";
+        }else{
+            msnErro.textContent = "";
+        }
+    }
+}
